1739d673d7d0baa1bbc39269e02f8cea77c4f509
Dong <xingdongdayu@users.noreply.github.com>
2017-08-03 10:17:26 -0700

    typo fix

--- src/app/guard/product.resolve.ts
+++ src/app/guard/product.resolve.ts
@@ -10,12 +10,12 @@ export class ProductResolve implements Resolve<Product> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product>|Promise<Product>|Product {
     // 获取当前路由中的参数，以此获取Product信息，然后这个Product信息会被携带在当前路由中，进入相应组件
     let productId: number = route.params["id"];
-    //模拟一下在数据库中根据id获取信息
+    // 模拟一下在数据库中根据id获取信息
     if(productId == 1){
       return new Product(1, "iPhone7");
     }else{
       // 没有获得想要的数据，重定向到home页。
-      // 重定向要用到Router对象，所以要在构造函数中注入Router，并且类要加装饰器@Injextable()
+      // 重定向要用到Router对象，所以要在构造函数中注入Router，并且类要加装饰器@Injectable()
       this.router.navigate((['/home']));
       return undefined;
     }
